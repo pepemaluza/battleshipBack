30b3feef6f7dd5193a71933d31dcf7fc2b2e0e3f
pedrogardelliano <pedrogardelliano@sirius.com.ar>
2022-06-02 16:53:31 -0300

    added some schemas

--- src/models/game.model.ts
+++ src/models/game.model.ts
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
-import UserSchema from "./user.model";
+import UserSchema, {UserDocument} from "./user.model";
+import * as Mongoose from "mongoose";
 
 interface GameDocument extends mongoose.Document {
     status: GameStatus;
+    player1: UserDocument;
+    player2: UserDocument;
     player1Ships: ShipsPosition;
     player2Ships: ShipsPosition;
-    name: string;
+    gameId: string;
 }
 
-interface position { x:number, y:number }
+interface position { x:number, y:number, horizontal: Boolean }
 
 interface ShipsPosition {
     Ship1: {pos1: position},
@@ -24,6 +27,31 @@ enum GameStatus {
     NOTSTARTED,
 }
 
+const ShipsSchema = new Mongoose.Schema({
+    Ships: {
+        Ship1: {pos1: {x: Number, y: Number, horizontal: Boolean}},
+        Ship2: {pos1: {x: Number, y: Number, horizontal: Boolean}, pos2: {x: Number, y: Number, horizontal: Boolean}},
+        Ship3: {
+            pos1: {x: Number, y: Number, horizontal: Boolean},
+            pos2: {x: Number, y: Number, horizontal: Boolean},
+            pos3: {x: Number, y: Number, horizontal: Boolean}
+        },
+        Ship4: {
+            pos1: {x: Number, y: Number, horizontal: Boolean},
+            pos2: {x: Number, y: Number, horizontal: Boolean},
+            pos3: {x: Number, y: Number, horizontal: Boolean},
+            pos4: {x: Number, y: Number, horizontal: Boolean}
+        },
+        Ship5: {
+            pos1: {x: Number, y: Number, horizontal: Boolean},
+            pos2: {x: Number, y: Number, horizontal: Boolean},
+            pos3: {x: Number, y: Number, horizontal: Boolean},
+            pos4: {x: Number, y: Number, horizontal: Boolean},
+            pos5: {x: Number, y: Number, horizontal: Boolean}
+        },
+    }
+})
+
 const GameSchema = new mongoose.Schema({
     player1: {
         type: UserSchema,
@@ -33,6 +61,14 @@ const GameSchema = new mongoose.Schema({
         type: UserSchema,
         required: true,
     },
+    player1Ships: {
+        type: ShipsSchema,
+        required: false,
+    },
+    player2Ships: {
+        type: ShipsSchema,
+        required: false,
+    },
     gameId: {
         type: String,
         required: true,
@@ -40,7 +76,7 @@ const GameSchema = new mongoose.Schema({
     status: {
         type: String,
         required: true,
-    }
+    },
 });
 
 export default mongoose.model<GameDocument>("Game", GameSchema);
